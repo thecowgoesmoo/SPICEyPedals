7a53b0c1b992cc51486d7bf4267e3ed6ee569ca1
Richard Moore <rkm31415@yahoo.com>
2025-07-14 22:23:13 -0700

    Create compareDists.m

---
+++ AnalysisScripts/compareDists.m
@@ -0,0 +1,17 @@
+%compareDists.m
+
+pdls = {'ProcoRatV6.cir' ...
+ 'IbanezTS9V2.cir' ...
+ 'ElectroharmonixBiggMuffPiV2.cir' ...
+ 'HarmonicPercolatorV1.cir'};
+
+figure;
+for k = 1:4
+	out = ngspiceRun(pdls{k});
+	plot(out(:,2),out(:,3),'LineWidth',2);
+	hold on;
+	grid on;
+end
+legend(pdls);
+xlabel('time (seconds)');
+ylabel('output (volts)');
